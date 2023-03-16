d532632413071f76ae254ba96ff202ceaac2baef
mariovelandiac <mariovelandiawork@gmail.com>
2023-03-16 11:32:32 -0500

    comenzando contenerización para deploy

--- kitchen/index.js
+++ kitchen/index.js
@@ -1,9 +1,27 @@
 const express = require('express'); // importación de express
 const app = express(); // aplicación de express
-app.use(express.json()); // habilitamos uso de JSON
+const cors = require('cors');
 const config = require('./config'); // archivo de configuración
 const routerApi = require('./network'); // router de la api de kitchen
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
+
+// habilitamos uso de JSON
+app.use(express.json());
+
+// Cors
+// array preparado para ingresar IPS de dominios permitidos para hacer peticiones. Por caso de uso, se deja abierta la API
+const whitelist = [];
+const options = {
+  origin: (origin, cb) => {
+    if (whitelist.includes(origin) || !origin) {
+      cb(null, true)
+    } else {
+      cb(new Error("domain not allowed"))
+    };
+  }
+};
+app.use(cors(options));
+
 // router
 routerApi(app);
 
--- warehouse/index.js
+++ warehouse/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const config = require('./config');
 const apiRouter = require('./components/network.warehouse');
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 
 // uso de formatos tipo JSON
 app.use(express.json());
+
+// Cors
+// array preparado para ingresar IPS de dominios permitidos para hacer peticiones. Por caso de uso, se deja abierta la API
+const whitelist = [];
+const options = {
+  origin: (origin, cb) => {
+    if (whitelist.includes(origin) || !origin) {
+      cb(null, true)
+    } else {
+      cb(new Error("domain not allowed"))
+    };
+  }
+};
+app.use(cors(options));
 
 // rutas
 app.use(apiRouter);
